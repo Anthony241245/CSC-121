bf2be164f3bd76f87fe3965017c1d47675afa497
Anthony <ajr320@outlook.com>
2025-01-23 10:54:45 -0500

    Update M2lab1_AnthonyRaynor.py

--- M2lab1_AnthonyRaynor.py
+++ M2lab1_AnthonyRaynor.py
@@ -6,19 +6,24 @@
 More_routes = 'y'
 route_num = 1
 fastest_time = []
+fastest_route_number = 0
 # While loop starts
 while More_routes  == 'y':
     route_distance = int(input(f"Enter route {route_num} distance (miles): "))
     route_speed = int(input(f"Enter route {route_num} speed (miles/hours): "))
     # Calc
     time_hr = route_distance / route_speed
-    fastest_time = time_hr * 60
+    time_minutes = time_hr * 60
     fastest_route_number = route_num
+
+     if time_minutes < fastest_time:
+        fastest_time = time_minutes
+        fastest_route_number = route_num
         
     route_num += 1    
     More_routes = input("More routes (y/n)?: ")
 
-    fastest_time.index = min()
+
         
     # Display
 print(f"Route {fastest_route_number} is the fastest; {fastest_time:.0f} mintues")
